0a3289eb107069370ff85bfd3a2e74ce454cd86d
Oscar Polania <opolania11@gmail.com>
2025-09-30 18:16:10 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
+// OneSignal SDK Worker (debe ir antes de cualquier otra cosa)
+try { importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js'); } catch(e) { /* ignore */ }
+
+// Opcional: versión para forzar actualización del SW cuando cambies este archivo
+// Cambia el número cuando quieras obligar a refrescar el SW en clientes
+self.__SW_VERSION__ = 'v1';
+
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
 

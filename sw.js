c8b7f5bb2521b27583ff9e0bf76b61975e915e26
Oscar Polania <opolania11@gmail.com>
2025-10-02 11:44:50 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-// Carga el worker de OneSignal (requerido para recibir Web Push)
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
-
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
 
 // Red primero; si falla, intenta caché (si existe)
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });

d306df9183c42609e2fb08d7367d24057852fde7
Oscar Polania <opolania11@gmail.com>
2026-04-08 15:10:59 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
 
-// Red primero; si falla, intenta caché (si existe)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
